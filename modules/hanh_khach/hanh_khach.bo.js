eca8dc7a2a189c972ab263792c6779dbe1944105
phuc899621 <phuc899621@gmail.com>
2025-12-12 15:14:44 +0700

    [Update] update return HanhKhach

--- modules/hanh_khach/hanh_khach.bo.js
+++ modules/hanh_khach/hanh_khach.bo.js
@@ -1,8 +1,9 @@
 export default class HanhKhachBO{
-    constructor({MaHK, HoTen, CMND, DienThoai}={}){
+    constructor({MaHK, HoTen, CMND, DienThoai,Email}={}){
         this.maHanhKhach = MaHK;
         this.hoTen = HoTen;
         this.cmnd = CMND;
         this.dienThoai = DienThoai;
+        this.email=Email;
     }
 }
